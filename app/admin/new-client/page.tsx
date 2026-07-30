d87fea299cdf85a4081bb87f15f3647cea89b200
jskillman1 <jourden@fonder.studio>
2026-07-30 09:09:48 -0700

    adding co-brand feature + minor fixes

--- app/admin/new-client/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EngagementForm } from "@/components/admin/EngagementForm";
-
-export default function NewClientPage() {
-  return (
-    <main className="min-h-screen bg-[var(--color-cream)] py-12 px-4">
-      <EngagementForm mode="create" />
-    </main>
-  );
-}
