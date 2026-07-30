7fc407ccd5f55c7ca04bb45ee2207e1432ee515c
jskillman1 <jourden@fonder.studio>
2026-07-30 13:10:39 -0700

    updating admin dash structure

--- app/api/admin/get-portal-copy/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { getPortalCopy } from "@/lib/portal-copy";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  const copy = await getPortalCopy();
-  return NextResponse.json(copy);
-}
