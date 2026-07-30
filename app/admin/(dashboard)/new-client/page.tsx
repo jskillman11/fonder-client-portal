a442949654fa368f06f38e4a7598a1315d2365dc
jskillman1 <jourden@fonder.studio>
2026-07-30 13:29:39 -0700

    updating dash usability

--- app/admin/(dashboard)/new-client/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EngagementForm } from "@/components/admin/EngagementForm";
-
-export default function NewClientPage() {
-  return (
-    <main className="py-12 px-4">
-      <EngagementForm mode="create" />
-    </main>
-  );
-}
