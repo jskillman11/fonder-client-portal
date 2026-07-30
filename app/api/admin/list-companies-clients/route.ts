da879b9bbb1802c86e946ae93c4ede07caf617a5
jskillman1 <jourden@fonder.studio>
2026-07-30 13:54:56 -0700

    adding magic links

--- app/api/admin/list-companies-clients/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { listCompanies, listClients } from "@/lib/companies-clients";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  const [companies, clients] = await Promise.all([listCompanies(), listClients()]);
-  return NextResponse.json({ companies, clients });
-}
--- app/api/admin/list-documents/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { listDocuments } from "@/lib/documents";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  const documents = await listDocuments();
-  return NextResponse.json({ documents });
-}
